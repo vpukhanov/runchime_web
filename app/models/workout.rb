cb6058f4d7576270e497fc96329a3d576ef8e698
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-01-27 19:51:45 +0300

    generate workout uuid

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 class Workout < ApplicationRecord
   has_many :scrobbles, dependent: :destroy
+
+  before_save :generate_id
+
+  private
+
+  def generate_id
+    update(id: SecureRandom.uuid) if id.blank?
+  end
 end
