20c3de1b65330c25074ef64032d8fa02089cf424
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-01-27 19:29:12 +0300

    add scrobble model

---
+++ db/migrate/20220127162733_create_scrobbles.rb
@@ -0,0 +1,14 @@
+class CreateScrobbles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :scrobbles do |t|
+      t.timestamp :uts
+      t.string :name
+      t.string :album
+      t.string :artist
+      t.string :image
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
